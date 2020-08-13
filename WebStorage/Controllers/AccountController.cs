6d95b525527d9213bdf7b351760e3d8214913e8d
yarimati <nikimihiki@gmail.com>
2020-08-13 17:42:00 +0300

    Initialize AccountController

---
+++ WebStorage/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebStorage.Models;
+
+namespace WebStorage.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+
+        public AccountController(UserManager<AppUser> usrMgr)
+        {
+            userManager = usrMgr;
+        }
+    }
+}
