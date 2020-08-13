2cd04e886ce7a0ce16319741896767c5f098e201
yarimati <nikimihiki@gmail.com>
2020-08-13 17:31:35 +0300

    Initialize

--- WebStorage/Startup.cs
+++ WebStorage/Startup.cs
@@ -15,7 +15,6 @@ namespace WebStorage
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
@@ -24,7 +23,6 @@ namespace WebStorage
 
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -34,7 +32,6 @@ namespace WebStorage
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
