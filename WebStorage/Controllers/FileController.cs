aa851100e74bd6ac7a1768e44decdd1e6379c6fb
yarimati <nikimihiki@gmail.com>
2020-08-14 19:58:12 +0300

    adaptive for mobile

--- WebStorage/Controllers/FileController.cs
+++ WebStorage/Controllers/FileController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
--- WebStorage/wwwroot/js/site.js
+++ WebStorage/wwwroot/js/site.js
@@ -1,4 +1,76 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿if ($(window).width() < 600) {
+    const listOfMobileElements = document.getElementsByClassName('forMobile');
+    for (var i = 0; i < listOfMobileElements.length; i++) {
+        listOfMobileElements[i].style.display = 'block';
+    }
 
-// Write your JavaScript code.
+}
+else {
+    const listOfComputerElements = document.getElementsByClassName('forComputers');
+    for (var i = 0; i < listOfComputerElements.length; i++) {
+        listOfComputerElements[i].style.display = 'block';
+    }
+}
+
+
+
+$(document).ready(function () {
+    $('form input').change(function () {
+        $('form p').text(this.files.length + " file(s) selected");
+    });
+});
+
+function fileInfo() {
+    const output = document.getElementById('output');
+    if (window.FileList && window.File) {
+        document.getElementById('file-selector').addEventListener('change', event => {
+            output.innerHTML = '';
+            totalWeight = 0;
+            for (const file of event.target.files) {
+
+                const li = document.createElement('li');
+                const name = file.name ? file.name : 'NOT SUPPORTED';
+                const size = file.size ? file.size : 'NOT SUPPORTED';
+                totalWeight += file.size;
+                li.textContent = `${name}, size: ${fileSize(size)}`;
+                output.appendChild(li);
+            }
+            allowUpload(totalWeight);
+            hideSpan();
+            showButton();
+            document.getElementById("totalSize").innerHTML = `Total size is ${fileSize(totalWeight)}`;
+        });
+    }
+}
+
+
+function fileSize(bytes) {
+    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes == 0) return 'n/a';
+    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    if (i == 0) return bytes + ' ' + sizes[i];
+    return (bytes / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i];
+};
+
+function hideSpan() {
+    document.getElementById("spanToHide").style.display = 'none';
+}
+function showButton() {
+    document.getElementById("uploadButton").removeAttribute("style");
+}
+
+
+
+function allowUpload(totalSize) {
+    if (totalSize > 20971520) {
+        window.alert(`Files cannot weight more than 20 MB,
+Your files weight ${fileSize(totalSize)}`);
+        window.location.reload(true);
+    }
+}
+
+function copyToClipboard() {
+    var textBox = document.getElementById("currentUrl");
+    textBox.select();
+    document.execCommand("copy");
+}
